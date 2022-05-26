0f2ba314b369bfd67850e017c3cd550049bae661
Trí Trần <20520327@gm.uit.edu.vn>
2022-05-26 21:16:10 +0700

    add view transaction to filter_search_dialog.xml

--- app/src/main/java/k15hkii/se114/bookstore/ui/dialog/filtersearch/FilterSearchCallBack.java
+++ app/src/main/java/k15hkii/se114/bookstore/ui/dialog/filtersearch/FilterSearchCallBack.java
@@ -3,4 +3,7 @@ package k15hkii.se114.bookstore.ui.dialog.filtersearch;
 import k15hkii.se114.bookstore.ui.base.INavigator;
 
 public interface FilterSearchCallBack extends INavigator {
+    void dismissDialog();
+
+    void openSearchResult();
 }
