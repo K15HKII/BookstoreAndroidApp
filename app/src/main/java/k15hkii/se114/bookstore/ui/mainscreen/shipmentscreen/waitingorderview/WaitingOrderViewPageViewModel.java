e06b1a6772ce43442322f516910bd9eb8b4671a2
Huyenne <nguyenkhanhhuyen051122@gmail.com>
2022-06-07 15:13:49 +0700

    .

--- app/src/main/java/k15hkii/se114/bookstore/ui/mainscreen/shipmentscreen/waitingorderview/WaitingOrderViewPageViewModel.java
+++ app/src/main/java/k15hkii/se114/bookstore/ui/mainscreen/shipmentscreen/waitingorderview/WaitingOrderViewPageViewModel.java
@@ -3,31 +3,27 @@ package k15hkii.se114.bookstore.ui.mainscreen.shipmentscreen.waitingorderview;
 import androidx.databinding.Observable;
 import androidx.databinding.ObservableField;
 import androidx.lifecycle.MutableLiveData;
+import k15hkii.se114.bookstore.ui.ViewModelMapper;
 import k15hkii.se114.bookstore.ui.mainscreen.shipmentscreen.orderitemsrecycleview.OrderViewViewModel;
 import k15hkii.se114.bookstore.utils.rx.SchedulerProvider;
 import k15hkii.se114.bookstore.ui.base.BaseViewModel;
 import k15hkii.se114.bookstore.ui.mainscreen.shipmentscreen.orderitemsrecycleview.OrderItemViewModel;
 
+import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class WaitingOrderViewPageViewModel extends BaseViewModel<WaitingOrderViewPageNavigator> implements Observable {
 
-    public final ObservableField<List<OrderViewViewModel>> listOrder = new ObservableField<>(
-            Arrays.asList(new OrderViewViewModel("240.000", "note", getOrderListItem(2)),
-                          new OrderViewViewModel("120.000", "note", getOrderListItem(1)))
-    );
+    public final ObservableField<List<OrderViewViewModel>> items = new ObservableField<>();
 
-    public List<OrderItemViewModel> getOrderListItem(int quantity) {
+    @Inject protected ViewModelMapper mapper;
 
-        List<OrderItemViewModel> orderListItem = new ArrayList<>();
+    private String userId;
 
-        for (int i = 0; i < quantity; i++) {
-            orderListItem.add(new OrderItemViewModel("Dac nhan tam"));
-        }
+    public void getData() {
 
-        return orderListItem;
     }
 
     public WaitingOrderViewPageViewModel(SchedulerProvider schedulerProvider) {
